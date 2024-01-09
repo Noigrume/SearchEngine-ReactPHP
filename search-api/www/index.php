6b87083f4f356eb8f2ec9a06d1db86ca68014868
unknown <simatovic.lucie@gmail.com>
2024-01-09 15:21:15 +0100

    endpoint of API, defining basic router

---
+++ search-api/www/index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require '../config.php';
+
+// basic outer
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($request) {
+    case '/SearchEngine-ReactPHP/search-api/www/search':
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            $controller = new \App\Controllers\SearchController();
+            echo $controller->getAllMusics($_GET['query'] ?? '');
+        } else {
+            http_response_code(405);
+            echo 'Method Not Allowed';
+        }
+        break;
+
+    default:
+        http_response_code(404);
+        echo '404 Not Found';
+        break;
+}
