00910c10e56dec82885feb67007f729754fd59b6
unknown <simatovic.lucie@gmail.com>
2024-01-09 20:44:50 +0100

    remove useless code & rename variables

--- search-api/src/Controllers/SearchController.php
+++ search-api/src/Controllers/SearchController.php
@@ -6,11 +6,9 @@ use App\Models\SearchModel;
 
 class SearchController
 {
-    protected $musicModel;
-
     public function searchProducts($query)
     {
-        $musics = new SearchModel();
-        return json_encode($musics->searchProduct($query));
+        $products = new SearchModel();
+        return json_encode($products->searchProduct($query));
     }
 }
