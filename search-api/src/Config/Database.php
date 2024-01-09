a9afdd1ba9077fa52163f3e946c6d9c43c37f870
unknown <simatovic.lucie@gmail.com>
2024-01-09 11:05:04 +0100

    add config file for global configuration (db access, path)

--- search-api/src/Config/Database.php
+++ search-api/src/Config/Database.php
@@ -6,20 +6,15 @@ class Database
 {
     public function getConnection()
     {
-        $host = '';
-        $db = '';
-        $user = '';
-        $pass = '';
-        $charset = 'utf8mb4';
 
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $dsn = 'mysql:host=' . BDD_HOST . ';dbname=' . BDD_NAME . ';charset=utf8';
         $options = [
             \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
             \PDO::ATTR_EMULATE_PREPARES   => false,
         ];
         try {
-            return new \PDO($dsn, $user, $pass, $options);
+            return new \PDO($dsn, BDD_USER, BDD_PASSWORD, $options);
         } catch (\PDOException $e) {
             throw new \PDOException($e->getMessage(), (int)$e->getCode());
         }
---
+++ search-api/yourConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+// Database configuration
+define('BDD_HOST', '');
+define('BDD_NAME', '');
+define('BDD_USER', '');
+define('BDD_PASSWORD', '');
+
+// Path settings
+define('ROOT_DIR', __DIR__);
+define('PUBLIC_DIR', ROOT_DIR . '/www');
+define('MODELS_DIR', ROOT_DIR . '/src/Models');
+define('CONTROLLERS_DIR', ROOT_DIR . '/src/Controllers');
