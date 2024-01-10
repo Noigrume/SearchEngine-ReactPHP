36d7c999d5d70a1e2353f0449452ba0dfb070be5
unknown <simatovic.lucie@gmail.com>
2024-01-10 23:41:49 +0100

    upgrade CSS but not done yet

--- react-search-app/src/App.js
+++ react-search-app/src/App.js
@@ -1,26 +1,31 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-import SearchBar from './SearchBar';
-import ProductList from './ProductList';
-import './App.css';
-function App() {
-    const [products, setProducts] = useState([]);
+import axios from "axios";
+import React, { useState } from "react";
+import SearchBar from "./components/SearchBar";
+import ProductList from "./components/ProductList";
+import "./App.css";
 
-    const handleSearch = async (query) => {
-        try {
-            const response = await axios.get(`http://localhost/SearchEngine-ReactPHP/search-api/www/search?query=${query}`);
-            setProducts(response.data);
-        } catch (error) {
-            console.error('Error fetching products:', error);
-        }
-    };
-        
-    return (
-        <div>
-            <SearchBar onSearch={handleSearch} />
-            <ProductList products={products} />
-        </div>
-    );
+function App() {
+  const [products, setProducts] = useState([]);
+
+  const handleSearch = async (query) => {
+    try {
+      const response = await axios.get(
+        `http://localhost/SearchEngine-ReactPHP/search-api/www/search?query=${query}`
+      );
+      console.log(response.data);
+      setProducts(response.data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  return (
+    <div>
+      <div class="product-header"></div>
+      <SearchBar onSearch={handleSearch} />
+      <ProductList products={products} />
+    </div>
+  );
 }
 
 export default App;
